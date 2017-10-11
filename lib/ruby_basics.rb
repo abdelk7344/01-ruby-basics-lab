bd4d0e7b70f471928c0b9fb70de7763ffeca807a
abdelk7344 <abdelmonemmoamk7344@hstat.org>
2017-10-11 00:13:58 +0000

    finished ruby basics lab

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,23 +1,23 @@
-def division
-    
+def division(num_one,num_two)
+    num_one/num_two
 end
 
-def argue
-  
+def argue(something)
+  something
 end
 
-def greeting
+def greeting(greeting,name)
 
 end
 
 def return_a_value
-    
+    "Nice"
 end
 
-def pizza_party
-    
+def pizza_party(normal="cheese")
+    normal
 end
 
-def whisper
-    
+def whisper(anyting)
+    anyting.downcase
 end
